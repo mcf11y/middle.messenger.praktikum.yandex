b2f01303c3438f83a325e80be4222d6df9b55d86
Andrey Lobanov <andrey.l12399@gmail.com>
2023-10-03 00:16:29 +0300

    sprint_4: added husky

--- test/justtest.test.ts
+++
@@ -1,9 +0,0 @@
-import chai from 'chai';
-
-describe("test", function () {
-  describe("sum", function () {
-    it("складываем два числа", function () {
-      chai.assert.equal(1, 1);
-    });
-  });
-});
